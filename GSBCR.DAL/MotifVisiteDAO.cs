364e8617ba5160ef3d4fd6bde47f2ff8b4ffc696
BraviMathieu <mathieu.bravi@free.fr>
2019-03-21 15:02:21 +0100

    MAJ des DAO

--- GSBCR.DAL/MotifVisiteDAO.cs
+++ GSBCR.DAL/MotifVisiteDAO.cs
@@ -15,13 +15,30 @@ namespace GSBCR.DAL
         public static MOTIF_VISITE FindById(string code)
         {
             //A faire : rechercher un motif visite par son nom
-            return null;
+            MOTIF_VISITE mot = null;
+            using (var context = new GSB_VisiteEntities())
+            {
+                //context.Configuration.LazyLoadingEnabled = false;
+                var req = from m in context.MOTIF_VISITE.Include("LesRapports")
+                          where m.MOT_CODE == code
+                          select m;
+                mot = req.SingleOrDefault<MOTIF_VISITE>();
+            }
+            return mot;
         }
 
         public static List<MOTIF_VISITE> FindAll()
         {
             //A faire : charger tous les motifs visite
-            return null;
+            List<MOTIF_VISITE> mots = null;
+            using (var context = new GSB_VisiteEntities())
+            {
+                //context.Configuration.LazyLoadingEnabled = false;
+                var req = from f in context.MOTIF_VISITE.Include("LesRapports")
+                          select f;
+                mots = req.ToList<MOTIF_VISITE>();
+            }
+            return mots;
         }
     }
 }
--- GSBCR.modele/GSB_Visite.Designer.cs
+++ GSBCR.modele/GSB_Visite.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'D:\Saves\C# Projet\GSBCR.modele\GSBCR.modele\GSB_Visite.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\mathieu.bravi\Desktop\GSBCR.modele\GSBCR.modele\GSB_Visite.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
