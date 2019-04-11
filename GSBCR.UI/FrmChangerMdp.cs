f018b5de0d6f04955031e3aad100f718c650f669
kyllian1212 <kyllian1212@live.fr>
2019-04-11 16:52:52 +0200

    fin du projet

--- GSBCR.UI/FrmChangerMdp.cs
+++ GSBCR.UI/FrmChangerMdp.cs
@@ -26,12 +26,12 @@ namespace GSBCR.UI
             string dictionnaire = "abcdefghijklmnopqrstuvwxyz";
             string dictionnaireMaj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string chiffre = "1234567890";
-            if(txt_ancien.Text.Equals("") || txt_nouveau.Text.Equals("") || txt_nouveau2.Text.Equals(""))
+            if (txt_ancien.Text.Equals("") || txt_nouveau.Text.Equals("") || txt_nouveau2.Text.Equals(""))
             {
-                MessageBox.Show("Un champ n'est pas renseigné","Erreur",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("Un champ n'est pas renseigné", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
-            { 
+            {
                 VISITEUR vis = null;
                 vis = Manager.ChargerVisiteur(UserId, txt_ancien.Text);
                 if (vis == null)
@@ -58,40 +58,51 @@ namespace GSBCR.UI
                             }
                             else
                             {
-                                string[]  dico = dictionnaire.Split();
+                                string[] dico = dictionnaire.Split();
                                 string[] dicoMaj = dictionnaireMaj.Split();
                                 string[] dicochiffre = chiffre.Split();
 
-                                for (int i=0; i<dictionnaire.Length;i++)
+                                for (int i = 0; i < dictionnaire.Length; i++)
                                 {
                                     if (txt_nouveau.Text.Contains(dico[i]))
                                     {
                                         for (int j = 0; j < dictionnaireMaj.Length; j++)
                                         {
-                                            if (txt_nouveau.Text.Contains(dicoMaj[i]))
+                                            if (txt_nouveau.Text.Contains(dicoMaj[j]))
                                             {
                                                 for (int k = 0; k < dicochiffre.Length; k++)
                                                 {
+                                                    if (txt_nouveau.Text.Contains(dicochiffre[k]))
+                                                    {
+                                                        Manager.UpdateVisiteurMdp(UserId, txt_ancien.Text, txt_nouveau.Text);
+                                                    }
+                                                    else
+                                                    {
+                                                        MessageBox.Show("erreur");
+                                                    }
 
                                                 }
 
                                             }
+                                            else
+                                            {
+                                                MessageBox.Show("erreur");
+                                            }
                                         }
 
                                     }
+                                    else
+                                    {
+                                        MessageBox.Show("erreur");
+                                    }
                                 }
-                                if (!txt_nouveau.Text.Contains(dictionnaire) || !txt_nouveau.Text.Contains(dictionnaireMaj) || !txt_nouveau.Text.Contains(chiffre))
-                                {
-                                    MessageBox.Show("Le mot de passe n'est pas suffisamment fort : min 6 caractères, une lettre majuscule, une lettre minuscule et un chiffre  ", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                }
-
                             }
 
-                    }
+                        }
 
+                    }
                 }
             }
-
 
         }
     }
